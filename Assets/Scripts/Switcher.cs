f9460813f67905d09613cb9b310fe9626f122970
ItzhakBista <165579089+ItzhakBista@users.noreply.github.com>
2025-11-10 14:17:10 +0200

    Added scene switcher

---
+++ Assets/Scripts/Switcher.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    void Update()
+    {
+        // 1 = מעבר לסצנה של הרכיבים (PartA)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("PartA");
+        }
+
+        // 2 = מעבר לסצנה של המיני-מפה (PartB)
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("PartB");
+        }
+    }
+}
